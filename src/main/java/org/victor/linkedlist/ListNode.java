003830ee1ad075b5faf9fc49ff7f427ab8ec67a9
Vignesh D <vigneshthevictor@gmail.com>
2025-09-16 20:16:17 +0530

    Leet : : Entity classes added.

---
+++ src/main/java/org/victor/linkedlist/ListNode.java
@@ -0,0 +1,7 @@
+package org.victor.linkedlist;
+
+ public class ListNode {
+      public int val;
+      public ListNode next;
+      ListNode(int x) { val = x; next = null; }
+ }
---
+++ src/main/java/org/victor/linkedlist/RandomListNode.java
@@ -0,0 +1,9 @@
+package org.victor.linkedlist;
+
+public class RandomListNode {
+
+      int label;
+      RandomListNode next, random;
+
+      RandomListNode(int x) { this.label = x; }
+};
