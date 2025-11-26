93e7474e61a2fb3145ba04e42536d5f64bae6dad
Vignesh D <vigneshthevictor@gmail.com>
2025-11-26 10:37:59 +0530

    Leet : : Pattern Problem commit 3. -. CombinedRightPyramid #total_91.

---
+++ src/main/java/org/victor/patterns/CombinedRightPyramid.java
@@ -0,0 +1,38 @@
+package org.victor.patterns;
+
+public class CombinedRightPyramid {
+
+    public static void print(int n) {
+
+        //upper
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = i; j <= n - 1; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
